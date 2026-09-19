e53b9d2fa592c6040342a15091e5a3141501959a
eireneysabelkarunungan-lgtm <eireneysabelkarunungan@gmail.com>
2026-09-19 11:55:24 +0800

    added project 1

--- Models/ProjectData.cs
+++ Models/ProjectData.cs
@@ -7,6 +7,15 @@ namespace BSIT31E2_Karunungan_Eirene_Portfolio.Models
         public static List<Project> All { get; } = new List<Project>
         {
             // I'Ll add projects added one at a time
+
+            new Project
+            {
+                Id = 1,
+                Title = "Draft Portfolio",
+                GithubUrl = "https://github.com/eireneysabelkarunungan-lgtm/IT_ELECTIVE_2_Midterm_A1_Karunungan_Eirene",
+                Description = "A practice portfolio using Razor views and Bootstrap.",
+                ThumbnailUrl = "https://placehold.co/600x400/FBF3E1/1F2A44?text=Draft+Portfolio"
+            },
         };
     }
 }
